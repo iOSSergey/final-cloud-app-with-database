ed26339db7267b5fc0c64d211ad67b67e84a6138
iOSSergey <live.on@hotmail.com>
2022-08-27 10:11:11 +0300

    Update settings.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -110,18 +110,68 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    submitted_anwsers  = extract_answers(request)
 
+    dummy_lesson = Lesson.objects.filter(course_id = course_id)
+    lesson_dummy_id = []
+
+    for l in dummy_lesson:
+        lesson_dummy_id.append(l.id)
+    # print("Lesson Id ===============> ",lesson_dummy_id)
+    # Create any data and add it to the context
+    question_list = Question.objects.filter(lesson_id__in = lesson_dummy_id)
+    total_marks = 0
+    marks_obtained = 0
+
+    for question in question_list:
+        print("Question ===========>" + question.content + "======> marks " + str(question.grade))
+        correct_ans = True
+        for choice in question.choice_set.all():
+            if (choice.id in submitted_anwsers) and (not choice.is_correct):
+                correct_ans = False
+                break
+        if Question.is_get_score(question, submitted_anwsers) and correct_ans:
+            marks_obtained+=question.grade
+
+        total_marks+=question.grade
+
+    percent_obtained = int(ceil((marks_obtained/total_marks)*100))
+    # return HttpResponse("Success" + str(percent_obatained), content_type='application/json')
+    # request.body["percent_obatained"] = percent_obatained
+    submission_id = uuid.uuid1()
+    print(submission_id)
+    context = {}
+    context['course_id'] = course_id
+    context['percent_obtained'] = percent_obtained
+    context['submission_id'] = submission_id
+    context['question_list'] = question_list
+    context['submitted_anwsers'] = submitted_anwsers
+    print("Submitted >>>>>>>>>>>>>>>>>>>>>>>>>>", submitted_anwsers)
+    # HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course_id,percent_obtained,submission_id)))
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_anwsers = []
+    print(request.method)
+    if request.method == 'GET':
+        return redirect('onlinecourse:index')
+    try:
+        print(request.POST)
+        for key in request.POST:
+            if key.startswith('choice'):
+                value = request.POST[key]
+                choice_id = int(value)
+                submitted_anwsers.append(choice_id)
+    except Exception as err:
+        print("Extract Answer error ===============> ", err)
+    print("Sbmitted Answers ==================>",submitted_anwsers)
+    
+    # return HttpResponse(submitted_anwsers, content_type='application/json')
+    return submitted_anwsers
+
+
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -130,7 +180,9 @@ def enroll(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
-
-
+# def show_exam_result(request, course_id, submission_id):
+#     context = {}
+#     context['course_id'] = course_id
+#     context['percent_obtained'] = request.body.percent_obatained
+#     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
